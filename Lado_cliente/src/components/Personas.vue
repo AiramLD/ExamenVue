55f079503f22a84103c39eea960a7c927df18860
AiramLD <airamluisdiaz629@gmail.com>
2024-02-19 18:45:19 +0000

    Cambuis

--- Lado_cliente/src/components/Personas.vue
+++ Lado_cliente/src/components/Personas.vue
@@ -6,8 +6,8 @@ const numPeople = ref(1);
 const checkNumPeople = () => {
   if (numPeople.value <= 0) {
     numPeople.value = 1;
-  } else if (numPeople.value > 10) {
-    numPeople.value = 10;
+  } else if (numPeople.value > 30) {
+    numPeople.value = 30;
   }
 };
 
@@ -17,8 +17,8 @@ const checkNumPeople = () => {
 <template>
   <div>
     <label for="numPeople">Número de personas:</label>
-    <input type="number" id="numPeople" v-model.number="numPeople" min="1" max="10" @input="checkNumPeople">
-    <input type="range" v-model.number="numPeople" min="1" max="10">
+    <input type="number" id="numPeople" v-model.number="numPeople" min="1" max="30" @input="checkNumPeople">
+    <input type="range" v-model.number="numPeople" min="1" max="30">
   </div>
 
 </template>
